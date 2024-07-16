1dfafc1047b5b1ec79f75ff11ba34546d080d9e9
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2024-07-16 15:55:31 +0200

    face-model

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+ title!:string ;
+ description!:string;
+ createdAt!:Date;
+ snaps!:number;
+ imageUrl!:string;
+ message!:string;
 
+ ngOnInit(): void {
+     this.title="Dragonball Z"; 
+     this.description="The best anime ever";
+     this.createdAt=new Date();
+     this.snaps=0;
+     this.imageUrl="https://i.pinimg.com/564x/00/1d/26/001d26c2688e8adadec15487cbf79ad0.jpg";
+     this.message="snap";
+ }
+
+
+  Onsnap():void{
+    this.snaps=(this.snaps+1)%2;
+    if(this.snaps==1){
+      this.message="oops unSnap";
+    }
+    else{
+      this.message="snap";
+}
+  }
 }
